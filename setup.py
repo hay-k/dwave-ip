49c200c4844bbedfb7e582cc8793afb8ed538306
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2020-10-02 17:36:42 +0400

    Configure setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as fh:
+    REQUIREMENTS = fh.read().split('\n')
+
+setup(name='dwaveip',
+      version='0.0.0',
+      description='A package to handle integers with Dwave',
+      author='Hayk Sargsyan',
+      # author_email='',
+      url='https://github.com/hay-k/dwave-ip',
+      pckages=['dwaveip'],
+      license='MIT License',
+      install_requires=REQUIREMENTS,
+      python_requires='>=3.6'
+      )
